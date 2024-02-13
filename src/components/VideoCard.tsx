93f516a694dea2f93eff374fc7e340fac3eb0114
Rushil Patel <rushildineshpatel2000@gmail.com>
2024-02-13 01:55:25 -0500

    created VideoCard component

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,19 @@
+export function VideoCard(props: any) {
+  return (
+    <div>
+      <img src={props.thumbnail} className="rounded-xl"></img>
+      <div className="grid grid-cols-12 pt-2">
+        <div className="col-span-1">
+          <img className={"rounded-full w-14 h-14"} src={props.thumb}></img>
+        </div>
+        <div className="col-span-11">
+          <div>{props.title}</div>
+          <div className="col-span-11 text-gray-400 text-base">{props.author}</div>
+          <div className="col-span-11 text-gray-400 text-base">
+            {props.views} | {props.timestamp}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { VideoCard } from "../components/VideoCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div>
+      <VideoCard title="Kimi No Kawa | Your Name" thumbnail="thumbnail.png" thumb="GitHub-Mark.png" author="Studio Gilbhi" views="130M" timestamp="5 days ago"></VideoCard>
+    </div>
+  );
 }
